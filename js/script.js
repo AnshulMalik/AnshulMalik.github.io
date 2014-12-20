f436781b0f64062b6607d27f4b2ff63e072b5e4b
Anshul <malikanshul29@gmail.com>
2014-12-20 22:29:39 +0530

    Added loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+$(window).load(function() {
+			// Animate loader off screen
+			$(".se-pre-con").fadeOut("slow");;
+		});
+
 $(document).ready(function() {
         $('.header').animate({'margin-left' : '32%'}, 400);
         $('.header').animate({'margin-left' : '30%'}, 100);
